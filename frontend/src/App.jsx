cea377fbad3ee32f2ae743ec9fce9fed5c8c45a8
Abhay0410 <kumarsinghabhay77@gmail.com>
2025-08-12 11:59:54 +0530

    files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-indigo-600 text-white">
+      <h1 className="text-4xl font-bold">Tailwind v4 is working 🚀</h1>
+    </div>
+  );
+}
